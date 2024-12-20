156aaad867b4ae39639848eb016bdb2d86e36bb4
marcosamambaia <marcosamambaia@gmail.com>
2024-12-20 02:49:45 -0300

    Atualizao final

--- EstudoDeCaso(Python).py
+++ EstudoDeCaso(Python).py
@@ -1,8 +1,19 @@
-# Codigo com comentarios explicando as linhas e funcionamentos para ajudar no entendimento do professor: 
+#***********************************************************************************************************
+#  
 # Aluno: Marco Antonio Monteiro Pedro
 # Data 19/12/2024
 # Modulo 'Computational Logic Using Python'
-
+#
+#***********************************************************************************************************
+#
+# Foi usado função 'Try/Except' para evitar entrada irregular de usuario,
+# evitando parar o programa por erro, tambem foi feita dentro de uma laço
+# 'While True' para caso seja digitado a entrada errada o usuario pode repetir
+# a tarefa .
+# Codigo com comentarios explicando as linhas e funcionamentos para ajudar
+# na correção e avaliação do professor:
+#
+#***********************************************************************************************************
 
 
 
@@ -16,16 +27,23 @@ estoque = []
 def menu():
     print("\n \n ******************************  CONTROLE DE ESTOQUE  ********************************\n Escolha sua opção:\n \n (1) Adicionar produto    (2) Atualizar produto  (3) Excluir produto\n (4) Visualizar Estoque   (5) Sair do sistema \n \n************************************************************************************")
     return int(input("Digite a opção desejada: "))  # Retorna a opção escolhida pelo usuário
-
+      
 # Função para adicionar um produto ao estoque
 def adicionar_produto():
-    nome_produto = input('Qual o nome do produto? ')
-    preco_produto = float(input('Qual o valor do produto? '))
-    quantidade_estoque = int(input('Qual a quantidade em estoque? '))
-    # Adiciona um dicionário com os detalhes do produto à lista de estoque
-    estoque.append({'Nome': nome_produto, 'Preço': preco_produto, 'Quantidade': quantidade_estoque})
-    print(f"Produto {nome_produto} adicionado com sucesso.\n")
-    time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+    while True:
+        try:
+            nome_produto = input('Qual o nome do produto? ')
+            preco_produto = float(input('Qual o valor do produto? '))
+            quantidade_estoque = int(input('Qual a quantidade em estoque? '))
+            # Adiciona um dicionário com os detalhes do produto à lista de estoque
+            estoque.append({'Nome': nome_produto, 'Preço': preco_produto, 'Quantidade': quantidade_estoque})
+            print(f"Produto {nome_produto} adicionado com sucesso.\n")
+            time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+            break  # Sai do loop se não houver exceção
+        except ValueError:
+            print('Entrada de usuário inválida. Por favor, cadastre novamente.')
+            time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+   
 
 # Função para atualizar os dados de um produto no estoque
 def atualizar_produto():
@@ -37,17 +55,31 @@ def atualizar_produto():
         contador += 1  # Incrementa o contador
     print("\n")
     time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
-    indice = int(input('Digite o índice do produto que deseja atualizar: ')) - 1
-    if 0 <= indice < len(estoque):  # Verifica se o índice é válido
-        preco_produto = float(input('Qual o novo valor do produto? '))
-        quantidade_estoque = int(input('Qual a quantidade atualizada em estoque? '))
-        # Atualiza o preço e a quantidade do produto
-        estoque[indice]['Preço'] = preco_produto
-        estoque[indice]['Quantidade'] = quantidade_estoque
-        print(f"Produto {estoque[indice]['Nome']} atualizado com sucesso.\n")
-    else:
-        print("Índice inválido. Tente novamente.\n")
+
+    while True:
+        try:
+            indice = int(input('Digite o índice do produto que deseja atualizar: ')) - 1
+            if 0 <= indice < len(estoque):  # Verifica se o índice é válido
+                break
+            else:
+                print("Índice inválido. Tente novamente.\n")
+        except ValueError:
+            print("Entrada inválida! Por favor, digite o numero correto do produto.\n")
+
+    while True:
+        try:
+            preco_produto = float(input('Qual o novo valor do produto? '))
+            quantidade_estoque = int(input('Qual a quantidade atualizada em estoque? '))
+            break
+        except ValueError:
+            print("Entrada inválida! Por favor, digite valores numéricos válidos.\n")
+
+    # Atualiza o preço e a quantidade do produto
+    estoque[indice]['Preço'] = preco_produto
+    estoque[indice]['Quantidade'] = quantidade_estoque
+    print(f"Produto {estoque[indice]['Nome']} atualizado com sucesso.\n")
     time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+
 
 # Função para excluir um produto do estoque
 def excluir_produto():
@@ -59,15 +91,22 @@ def excluir_produto():
         contador += 1  # Incrementa o contador
     print("\n")
     time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
-    indice = int(input('Digite o índice do produto que deseja excluir: ')) - 1
-    if 0 <= indice < len(estoque):  # Verifica se o índice é válido
-        nome_produto = estoque[indice]['Nome']
-        # Remove o produto do estoque
-        del estoque[indice]
-        print(f"Produto {nome_produto} excluído com sucesso.\n")
-    else:
-        print("Índice inválido. Tente novamente.\n")
+
+    while True:
+        try:
+            indice = int(input('Digite o índice do produto que deseja excluir: ')) - 1
+            if 0 <= indice < len(estoque):  # Verifica se o índice é válido
+                nome_produto = estoque[indice]['Nome']
+                # Remove o produto do estoque
+                del estoque[indice]
+                print(f"Produto {nome_produto} excluído com sucesso.\n")
+                break  # Sai do loop se o produto for excluído com sucesso
+            else:
+                print("Índice inválido. Tente novamente.\n")
+        except ValueError:
+            print("Entrada inválida! Por favor, digite um número correto do produto.\n")
     time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+
 
 # Função para visualizar o estoque atual
 def visualizar_estoque():
@@ -95,17 +134,22 @@ def sair_do_sistema():
 
 # Loop principal do programa
 while True:
-    escolha = menu()  # Exibe o menu e obtém a escolha do usuário
-    if escolha == 1:
-        adicionar_produto()
-    elif escolha == 2:
-        atualizar_produto()
-    elif escolha == 3:
-        excluir_produto()
-    elif escolha == 4:
-        visualizar_estoque()
-    elif escolha == 5:
-        sair_do_sistema()
-    else:
-        print("Opção inválida. Tente novamente.\n")
+    try:
+        escolha = menu()  # Exibe o menu e obtém a escolha do usuário
+        if escolha == 1:
+            adicionar_produto()
+        elif escolha == 2:
+            atualizar_produto()
+        elif escolha == 3:
+            excluir_produto()
+        elif escolha == 4:
+            visualizar_estoque()
+        elif escolha == 5:
+            sair_do_sistema()
+        else:
+            print("Opção inválida. Tente novamente.\n")
+            time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+    except ValueError:
+        print('Entrada de usuario invalida, digite apenas o numero da opção desejada!')
         time.sleep(2)  # Pausa para exibir a mensagem por 2 segundos
+    
